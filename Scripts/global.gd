16669b995564880f305319b9b0f1e9f8eadc8fd5
Olbap14 <mico2p@hotmail.com>
2017-08-12 10:23:19 +0200

    Starting ask_for_resources()
    Starting ask_for_resources(), the first player action fucntion. I just
    make a subordinate function 'daysToComeBack()' wich calc the days
    soldiers will take to combe back

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -27,6 +27,8 @@ var hunger_troopsDown
 #use for rationing resources
 var rationing = false
 
+#days that soldiers will take to come back with resources 
+var daysToComeBack
 
 func _ready():
 	set_process_input(true)
@@ -49,13 +51,13 @@ func set_enemyM(cEnemyM):
 
 #moral decrement by hunger, take in count moral and days_starving
 func hunger_moralDown(any_resources , any_troops , any_moral):
-	if(days_starving == 1 and any_resources > 0):
+	if(any_resources > 0):
 		hunger_moralDown = int(round((pow(10,(any_moral/100))) * pow(1.1,days_starving) * randF() * (any_troops/any_resources)) )
 	return hunger_moralDown
 
 #troops decrement by hunger, take in count troops and days starving
 func hunger_troopsDown(any_resources , any_troops):
-	if(days_starving == 1 and any_resources > 0):
+	if(any_resources > 0):
 		hunger_troopsDown = int(round(((pow(10,days_starving/10) * randF()) * 0.1) * ((any_troops/30)*randF() ) * (any_troops/any_resources)))
 	else:
 		hunger_troopsDown = int(round(((pow(10,days_starving/10) * randF()) * 0.1) * ((any_troops/30)*randF() )))
@@ -84,8 +86,12 @@ func valuesOverZ():
 	if(enemyR < enemyT):
 		enemyR=0
 
+#calcs the days soldiers will take to come back
+func daysToComeBack():
+	daysToComeBack = int(round((log(enemyT) * randF() * 2 )+ 1))
+	return daysToComeBack
 ##When a day ends: incrises day, and take resources taking in count troops, if 
-##	there is not enough resources moral will go down and soldiers may die
+##	there is not enough resources moral will go down and soldiers may die (and same for enemy)
 func new_day ():
 	if(playerHasMove and enemyHasMove):
 		day += 1
@@ -112,3 +118,5 @@ func new_day ():
 			enemyM -= hunger_moralDown(enemyR , enemyT , enemyM)
 			enemyT -= hunger_troopsDown(enemyR , enemyT)
 		valuesOverZ()
+
+func ask_for_resources():
